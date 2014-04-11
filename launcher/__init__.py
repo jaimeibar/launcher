d01ba446f245b7875e6e57f9e640d5b532c5ec2e
Jaime Ibar <jim2k7@gmail.com>
2014-04-11 16:00:29 +0200

    Added list of modules.

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -1,3 +1,7 @@
+from launcher.makinator import do_openstack_login, get_flavour_list
+from launcher.makinator import get_image_name, get_security_group
+from launcher.makinator import get_keypairs, launch_virtual_machines
+
 __all__ = ['do_openstack_login', 'get_flavour_list', 'get_image_name',
            'get_security_group', 'get_keypairs', 'launch_virtual_machines']
 __version__ = '0.1'
